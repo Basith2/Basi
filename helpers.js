e342c02ef4c0b418580f7b94b4ee56c3aec39bbf
Basi <93530819+Basith2@users.noreply.github.com>
2021-11-02 23:23:41 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *Julie Mwol*:  ```" + msg + "```"
+    return "✅ *malaak*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *Julie Mwol*:  ```" + msg + "```"
+    return "🛑 *malaak*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *Julie Mwol*:  ```" + msg + "```"
+    return "⏺️ *malaak*:  ```" + msg + "```"
 }
 
 
